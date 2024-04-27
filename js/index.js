d9af034af165f78630892e80024317dd90abe553
Joseph <56847381+josephtrandev@users.noreply.github.com>
2024-04-27 18:52:37 -0500

    Rewrote timer logic to be more accurate and improved pause responsiveness

--- js/index.js
+++ js/index.js
@@ -1,43 +1,46 @@
-let mins = .1;
-let secs = mins * 60;
-minutes = document.getElementById("minutes");
-seconds = document.getElementById("seconds");  
+let myTimer;
+let totalTime = 1500; // value in seconds, default of 1500 is 25 minutes
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
 let timerRunning = false;
 let alarm = new Audio('./sound/basic-alarm.mp3');
 
-function Decrement() {
+function clock() {
     if (document.getElementById) {
-        if (seconds < 59) {
-            seconds.value = secs;
-        }
-        else {
-            minutes.value = getMinutes();
-            seconds.value = getSeconds();
-            formatTime();
-        }
+        myTimer = setInterval(myClock, 1000);
 
-        if (mins < 1) {
-            minutes.style.color = "red";
-            seconds.style.color = "red";
-        }
-        else {
-            minutes.style.color = "white";
-            seconds.style.color = "white";
-        }
-        
-        if (mins < 0) {
-            alarm.play();
-            minutes.value = 0;
-            seconds.value = 0;
+        function myClock() {
+            --totalTime
+            secs = totalTime % 60; // Seconds that cannot be written in minutes
+            let secondsInMinutes = (totalTime - secs) / 60; // Gives the seconds that COULD be given in minutes
+            mins = secondsInMinutes % 60;
+
+            minutes.value = mins;
+            seconds.value = secs;
+
+            if (mins < 1) {
+                minutes.style.color = "red";
+                seconds.style.color = "red";
+            }
+            else {
+                minutes.style.color = "white";
+                seconds.style.color = "white";
+            }
+
             formatTime();
-            alert('time up');
-            alarm.pause();
-            alarm.load();
-        }
-        else {
-            secs--;
-            if (timerRunning) {
-                timer = setTimeout('Decrement()', 1000);
+
+            if (totalTime < 0) {
+                clearInterval(myTimer);
+
+                minutes.value = 0;
+                seconds.value = 0;
+                formatTime();
+
+                alarm.play();
+                alert('time up');
+                alarm.pause();
+                alarm.load();
             }
         }
     }
@@ -45,22 +48,15 @@ function Decrement() {
 
 function startCountdown() {
     if(timerRunning === false) {
-        setTimeout('Decrement()', 60);
+        clock();
     }
+    
     timerRunning = true;
 }
 
 function stopCountdown() {
+    clearInterval(myTimer);
     timerRunning = false;
-}
-
-function getMinutes() {
-    mins = Math.floor(secs / 60);
-    return mins;
-}
-
-function getSeconds() {
-    return secs - Math.round(mins * 60);
 }
 
 function formatTime() {
@@ -74,45 +70,27 @@ function formatTime() {
 
 function setPomodoro() {
     stopCountdown();
-    document.getElementById("set-pomodoro").classList.add("btnActive");
-    document.getElementById("set-short").classList.remove("btnActive");
-    document.getElementById("set-long").classList.remove("btnActive");
-    minutes.style.color = "white";
-    seconds.style.color = "white";
     
-    mins = 25;      // Eventually allow user to set time
-    minutes.value = mins;
-    secs = mins * 60;
+    totalTime = 1500;      // 25 min, Eventually allow user to set time
+    minutes.value = 25;
     seconds.value = 0;
     formatTime();    
 }
 
 function setShortBreak() {
     stopCountdown();
-    document.getElementById("set-short").classList.add("btnActive");
-    document.getElementById("set-pomodoro").classList.remove("btnActive");
-    document.getElementById("set-long").classList.remove("btnActive");
-    minutes.style.color = "white";
-    seconds.style.color = "white";
 
-    mins = 5;      // Eventually allow user to set time
-    minutes.value = mins;
-    secs = mins * 60;
+    totalTime = 300;      // 5 min, Eventually allow user to set time
+    minutes.value = 5;
     seconds.value = 0;
     formatTime();
 }
 
 function setLongBreak() {
     stopCountdown();
-    document.getElementById("set-long").classList.add("btnActive");
-    document.getElementById("set-short").classList.remove("btnActive");
-    document.getElementById("set-pomodoro").classList.remove("btnActive");
-    minutes.style.color = "white";
-    seconds.style.color = "white";
 
-    mins = 30;      // Eventually allow user to set time
-    minutes.value = mins;
-    secs = mins * 60;
+    totalTime = 1800;      // 30 min, Eventually allow user to set time
+    minutes.value = 30;
     seconds.value = 0;
     formatTime();
 }
